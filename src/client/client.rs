252157b06fc5ab60ea7150b7caf142d1a91c5007
Barlords <basile.pulin@gmail.com>
2023-02-10 10:44:28 +0100

    com with server

--- src/client/client.rs
+++ src/client/client.rs
@@ -8,26 +8,41 @@ use crate::messages::{SubscribeError, SubscribeResult};
 use crate::MessageType;
 
 pub struct Client {
-    server_connected_ip: String,
-    game_version: u8,
-    stream: Option<TcpStream>,
-    listener: Option<TcpListener>
+    pub server_connected_ip: String,
+    pub game_version: u8,
+    pub is_connected: bool,
+    pub stream: Option<TcpStream>,
+    pub listener: Option<TcpListener>
 }
 
 impl Client {
     pub fn connect(&mut self, server_ip: String) -> Result<(), ClientConnexionError> {
         let stream = TcpStream::connect(&server_ip);
-        let listener = TcpListener::bind("127.0.0.1:7878");
-        self.listener = Option::from(self.check_listener(listener));
-        self.stream = Option::from(self.check_stream(stream));
-        self.server_connected_ip = server_ip;
-        println!("You are connected to the server");
+        match self.check_stream(stream) {
+            Ok(_) => {
+                self.server_connected_ip = server_ip;
+                println!("You are connected to the server");
 
-        self.send_message(MessageType::Hello)?;
+                self.send_message(MessageType::Hello)?;
 
-        self.server_communication()?;
+                while self.is_connected {
+                    self.wait_server_message()?;
+                }
 
-        Ok(())
+                Ok(())
+            }
+            Err(_) => {
+                Err(ClientConnexionError)
+            }
+        }
+
+
+
+
+
+
+
+
     }
 
     fn send_message(&mut self, message: MessageType) -> Result<(), ClientConnexionError> {
@@ -57,60 +72,62 @@ impl Client {
         }
     }
 
-    fn server_communication(&mut self) -> Result<(), ClientConnexionError> {
-        let listener = self.listener.take().ok_or(ClientConnexionError)?;
-        for stream in listener.incoming() {
-            println!("Incoming stream");
-            let mut stream = self.check_stream(stream);
+    fn wait_server_message(&mut self) -> Result<(), ClientConnexionError> {
+        let message = self.read_messages();
+        let message_type = MessageType::deserialize_from_string(&message?);
+        self.interpret_message(&message_type);
 
-            let message = self.read_messages(&mut stream);
-
-            let message_type = MessageType::deserialize_from_string(&message);
-
-            self.interpret_message(&message_type);
-        }
         Ok(())
     }
 
-    fn check_stream(&mut self, stream: io::Result<TcpStream>) -> TcpStream {
-        match stream {
-            Ok(ok) => { return ok; }
-            Err(_) => { panic!("Error when trying to connect on {}", self.server_connected_ip); }
+    fn check_stream(&mut self, stream: io::Result<TcpStream>) -> Result<(), ClientConnexionError> {
+        return match stream {
+            Ok(ok) => {
+                self.stream = Option::from(ok);
+                self.is_connected = true;
+                println!("You are connected on {} server", self.server_connected_ip);
+                Ok(())
+            }
+            Err(_) => {
+                println!("Error when trying to connect on {}", self.server_connected_ip);
+                Err(ClientConnexionError)
+            }
         }
     }
 
-    fn check_listener(&mut self, listener: io::Result<TcpListener>) -> TcpListener {
-        match listener {
-            Ok(ok) => { return ok; }
-            Err(_) => { panic!("Error when trying to listen on port 7878");}
-        }
+    fn read_messages(&mut self) -> Result<String, ClientConnexionError> {
+        let message_size = self.read_message_size();
+        let message = self.read_message_content(message_size?);
+        message
     }
 
-    fn read_messages(&mut self, stream: &mut TcpStream) -> String {
-        let message_size = self.read_message_size(stream);
-        let message = self.read_message_content(stream, message_size);
-        return message;
-    }
-
-    fn read_message_size(&self, stream: &mut TcpStream) -> u32 {
+    fn read_message_size(&mut self) -> Result<u32, ClientConnexionError> {
         let mut buf_n = [0u8; 4];
-        let res = stream.read_exact(&mut buf_n);
+        let res = self.stream.take().ok_or(ClientConnexionError)?.read_exact(&mut buf_n);
         match res {
-            Ok(_) => {}
-            Err(_) => {panic!("Error when read message 'size message' of server")}
+            Ok(_) => {
+                Ok(u32::from_be_bytes(buf_n))
+            }
+            Err(_) => {
+                println!("Error when read message 'size message' of server");
+                Err(ClientConnexionError)
+            }
         }
-        return u32::from_be_bytes(buf_n);
     }
 
-    fn read_message_content(&self, stream: &mut TcpStream, message_size: u32) -> String {
+    fn read_message_content(&mut self, message_size: u32) -> Result<String, ClientConnexionError> {
         let mut buf = Vec::<u8>::new();
         buf.resize(message_size as usize, 0);
-        let res = stream.read_exact(&mut buf);
+        let res = self.stream.take().ok_or(ClientConnexionError)?.read_exact(&mut buf);
         match res {
-            Ok(_) => {}
-            Err(_) => {panic!("Error when read message 'content message' of server")}
+            Ok(_) => {
+                Ok(String::from_utf8_lossy(&buf).to_string())
+            }
+            Err(_) => {
+                println!("Error when read message 'content message' of server");
+                Err(ClientConnexionError)
+            }
         }
-        return String::from_utf8_lossy(&buf).to_string();
     }
 
     pub fn interpret_message(&mut self, message_type: &MessageType) {
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,25 @@ mod error;
 
 use std::env;
 use messages::MessageType;
+use crate::client::client::Client;
+use crate::error::ClientConnexionError;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-
+    //let query = &args[1];
+    
+    let mut client: Client = Client{
+        server_connected_ip: "".to_string(),
+        game_version: 1,
+        is_connected: false,
+        stream: None,
+        listener: None,
+    };
+    
+    match client.connect("127.0.0.1:7878".to_string()) {
+        Ok(_) => { println!("Connection OK") }
+        Err(_) => { println!("Error when trying to connect") }
+    }
+    
 }
