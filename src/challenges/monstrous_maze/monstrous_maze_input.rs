5f2bc691a664b952b5e5b7598b5f611a96c9e53e
matthieudupas <matthieudupas@outlook.fr>
2023-02-11 11:57:28 +0100

    Monstrous maze

--- src/challenges/monstrous_maze/monstrous_maze_input.rs
+++ src/challenges/monstrous_maze/monstrous_maze_input.rs
@@ -3,5 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(PartialEq, Serialize, Deserialize, Debug)]
 pub struct MonstrousMazeInput {
     pub grid: String,
-    pub endurance: u32,
+    pub endurance: u8,
 }
